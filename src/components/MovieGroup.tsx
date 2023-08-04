cc1d768b1c0a6fb4b2bd83762fcbbd3f32adb17e
Victor Hugo Fernandes <victorh.fernandes.o@gmail.com>
2023-08-03 22:34:04 -0300

    add default poster

--- src/components/MovieGroup.tsx
+++ src/components/MovieGroup.tsx
@@ -20,20 +20,21 @@ function MovieGroup({ movies, hideRating }: Props) {
   return (
     <>
       {movies.map((item, index) => (
-        <div
-          key={index}
-          className={hideRating ? "flex-movie-no-rating" : "flex-movie"}
-        >
+        <div key={index} className="flex-movie">
           {item.error ? (
             <div className="error">{item.error}</div>
           ) : (
             <>
-              <img
-                alt={item.movieName}
-                className="moviePoster"
-                src={`https://image.tmdb.org/t/p/w500${item.moviePoster}`}
-              />
-              <div className="grid-rating">
+              {item.moviePoster ? (
+                <img
+                  alt={item.movieName}
+                  className="moviePoster"
+                  src={`https://image.tmdb.org/t/p/w500${item.moviePoster}`}
+                />
+              ) : (
+                <div className="no-poster">{item.movieName}</div>
+              )}
+              <div className={hideRating ? "grid-no-rating" : "grid-rating"}>
                 <Stars contStars={hideRating ? 0 : item.movieRating} />
                 {item.isHalf && !hideRating ? (
                   <img className="rating-icon" src={half} />
