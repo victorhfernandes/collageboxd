ee606bfe55377728fe52072cf7380c2a632dd67c
Victor Hugo Fernandes <victorh.fernandes.o@gmail.com>
2023-08-08 00:46:08 -0300

    add a href to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,11 @@
 import "./Footer.css";
 
 const Footer = () => {
-  return <footer>Made by Victor Hugo Fernandes</footer>;
+  return (
+    <footer>
+      Made by{" "}
+      <a href="https://github.com/victorhfernandes">Victor Hugo Fernandes</a>
+    </footer>
+  );
 };
 export default Footer;
