9b0b3c7b8ea961b141c54f1982fb14e02ff01835
Victor Hugo Fernandes <victorh.fernandes.o@gmail.com>
2023-12-02 18:12:30 -0300

    change About

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -12,10 +12,37 @@ const About = () => {
         this project that creates an image of what you have logged on Letterboxd
         in a month or in a year of your choice.
       </InfoItems>
-      <InfoItems title="Small Screens">
-        For some reason that I don't know (I’m working on it) when downloading
-        the collage on small screens the ratings are stretched, there are two
-        solutions for this:
+      <InfoItems title="How does Collageboxd work?">
+        First you need a Letterboxd account, you will put your username in the
+        field, select the period and year, wait until load and finally click on
+        the download button. <br />
+        If you are a nerd and want to know how it really work the code is right
+        here:{" "}
+        <Anchor href="https://github.com/victorhfernandes/collageboxd">
+          GitHub Repo
+        </Anchor>
+      </InfoItems>
+      <InfoItems title="Why is it taking so long to load? 🫤">
+        Everything in this site is upload in a free server so sometimes it can
+        be a little slow, here's what you can do about it:
+        <ul>
+          <li>
+            Close the tab and open again, if doesn't work close the browser and
+            try again, sometimes it really help
+          </li>
+          <li>
+            If you are using <b>Safari</b> it will be a little slower than other
+            browsers because of the way it's built.
+          </li>
+          <li>
+            Also the All Year function generally doesn't work on <b>Safari</b>,
+            try another browser.
+          </li>
+        </ul>
+      </InfoItems>
+      <InfoItems title="Am I crazy or do the rating stars look weird???">
+        Yeah, it's impossible to unsee once you notice, i don’t have no idea why
+        this happens in the download image, but you can do this:
         <ul>
           <li>
             Put your mobile browser in{" "}
@@ -29,6 +56,7 @@ const About = () => {
             the ratings, selecting the Hide Rating option.
           </li>
         </ul>
+        If you don’t know what we are talking about just ignore this.
       </InfoItems>
     </div>
   );
